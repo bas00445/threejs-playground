f86a03a6ef1c1c1c11c15f834222334d9c65dfcd
bas.phat <overtone00445@gmail.com>
2024-05-25 16:23:27 +0700

    setup tailwindcss with storybook

---
+++ src/stories/BasicScene/BasicScene.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "BasicScene",
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta;
+
+export default meta;
+
+export const Primary: StoryObj = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+  render: () => {
+    return <div className="bg-red-500 p-4">Hello</div>;
+  },
+};
